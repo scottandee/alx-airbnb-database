4cc900eed92413a4b5a8e0a925a6a7a5aa3c0485
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-09-03 18:36:12 +0100

    Add database index script

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,16 @@
+-- User Table
+CREATE INDEX idx_user_email ON User(email);
+
+-- Booking Table
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+CREATE INDEX idx_booking_date ON Booking(booking_date);
+
+-- Property Table
+CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX idx_property_price ON Property(price);
+```
+
+EXPLAIN SELECT * FROM Booking WHERE user_id = 1;
+
+ANALYZE SELECT * FROM Booking WHERE user_id = 1;
