3f8e834743878cc9727cca726e5522431d889237
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-09-03 18:28:44 +0100

    Add performance script

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -7,3 +7,27 @@ LEFT JOIN properties
     ON bookings.property_id = properties.id
 LEFT JOIN payments
     ON bookings.id = payments.booking_id;
+
+-- Analysis of query with EXPLAIN
+EXPLAIN SELECT *
+FROM bookings
+LEFT JOIN users
+    ON bookings.user_id = users.id
+LEFT JOIN properties
+    ON bookings.property_id = properties.id
+LEFT JOIN payments
+    ON bookings.id = payments.booking_id;
+
+-- Refactored Query
+SELECT 
+      bookings.id AS booking_id,
+      users.name AS user_name,
+      properties.name AS property_name,
+      payments.amount AS payment_amount
+FROM bookings
+INNER JOIN users
+        ON bookings.user_id = users.id
+INNER JOIN properties
+        ON bookings.property_id = properties.id
+INNER JOIN payments
+        ON bookings.id = payments.booking_id;
